57b024d821b4761b9c29693e4a7bd0949b3be43f
Nerimantas7 <nerimantas.tarabilda@stud.techin.lt>
2024-12-08 20:29:02 +0200

    Added Axios, React-Router-dom, CategoryService.js and uodated Recipe.jsx

--- Front-end/src/components/Recipe.jsx
+++ Front-end/src/components/Recipe.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const RecipeComponent = (props) => {
   const { recipeName, setRecipeName } = useState("");
   const { ingredients, setIngredients } = useState("");
   const { steps, setSteps } = useState("");
-  const { image, setImage } = useState("");
+  const { imageUrl, setImageUrl } = useState("");
   const { categoryId, setCategoryId } = useState("");
   const { categories, setCategories } = useState([]);
 
@@ -14,51 +15,83 @@ const RecipeComponent = (props) => {
   const [errors, setErrors] = useState({
     recipeName: "",
     ingredients: "",
-    image: "",
     steps: "",
+    imageUrl: "",
     recipeCategory: "",
   });
 
-// Function to save added or updated data from form
-function saveOrUpdateRecipe(e) {
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    if (id) {
+      getRecipe(id)
+        .then((response) => {
+          console.log(response.data);
+          setRecipeName(response.data.recipeName);
+          setIngredients(response.data.ingredients);
+          setSteps(response.data.steps);
+          setImageUrl(response.data.imageUrl);
+          setCategoryId(response.data.categoryId);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    getAllCategories()
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
+
+  // Function to save added or updated data from form
+  function saveOrUpdateRecipe(e) {
     e.preventDefault();
 
-    if (validateForm()) { // Add form validation check
-        const book = { recipeName, ingredients, steps, image, categoryId };
+    if (validateForm()) {
+      // Add form validation check
+      const book = { recipeName, ingredients, steps, imageUrl, categoryId };
 
-        if (id) {
-            // Add a confirmation dialog
-            if (window.confirm('Are you sure to update this recipe?')) {
-                console.log(recipe);
+      if (id) {
+        // Add a confirmation dialog
+        if (window.confirm("Are you sure to update this recipe?")) {
+          console.log(recipe);
 
-                updateRecipe(id, recipe).then((response) => {
-                    console.log(response.data);
-                    navigator('/recipes');      //not exist yet
-                }).catch(error => {
-                    console.error(error);
-                });
-            } else {
-                console.log('Update operation cancelled');
-            }
+          updateRecipe(id, recipe)
+            .then((response) => {
+              console.log(response.data);
+              navigator("/recipes"); //not exist yet
+            })
+            .catch((error) => {
+              console.error(error);
+            });
         } else {
-            // Add a confirmation dialog
-            if (window.confirm('Are you want to save this recipe?')) {
-                console.log(recipe);
-
-                createRecipe(recipe).then((response) => {
-                    console.log(response.data);
-                    navigator('/recipes');
-                }).catch(error => {
-                    console.error(error);
-                });
-            } else {
-                console.log('Save operation cancelled');
-            }
+          console.log("Update operation cancelled");
         }
-    }
-}
+      } else {
+        // Add a confirmation dialog
+        if (window.confirm("Are you want to save this recipe?")) {
+          console.log(recipe);
 
-// Function to check the form data
+          createRecipe(recipe)
+            .then((response) => {
+              console.log(response.data);
+              navigator("/recipes");
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        } else {
+          console.log("Save operation cancelled");
+        }
+      }
+    }
+  }
+
+  // Function to check the form data
   function validateForm() {
     let valid = true;
 
@@ -66,135 +99,153 @@ function saveOrUpdateRecipe(e) {
     const errorsCopy = { ...errors };
 
     if (recipeName.trim()) {
-        errorsCopy.recipeName = '';
+      errorsCopy.recipeName = "";
     } else {
-        errorsCopy.recipeName = 'Recipe name is required';
-        valid = false;
+      errorsCopy.recipeName = "Recipe name is required";
+      valid = false;
     }
 
     if (ingredients.trim()) {
-        errorsCopy.ingredients = '';
+      errorsCopy.ingredients = "";
     } else {
-        errorsCopy.ingredients = 'Ingredients are required';
-        valid = false;
+      errorsCopy.ingredients = "Ingredients are required";
+      valid = false;
     }
 
     if (steps.trim()) {
-        errorsCopy.steps = '';
+      errorsCopy.steps = "";
     } else {
-        errorsCopy.steps = 'Steps are required';
-        valid = false;
+      errorsCopy.steps = "Steps are required";
+      valid = false;
     }
 
     if (categoryId) {
-        errorsCopy.recipeCategory = '';
+      errorsCopy.recipeCategory = "";
     } else {
-        errorsCopy.recipeCategory = 'Select category';
-        valid = false;
+      errorsCopy.recipeCategory = "Select category";
+      valid = false;
     }
 
     setErrors(errorsCopy);
 
     return valid;
-}
+  }
 
-const handleCancel = () => {
-    navigator('/');
-};
+  const handleCancel = () => {
+    navigator("/");
+  };
 
-function pageTitle() {
+  function pageTitle() {
     if (id) {
-        return <h3 className='text-center'>Update Recipe</h3>;
+      return <h3 className="text-center">Update Recipe</h3>;
     } else {
-        return <h3 className='text-center'>Add Recipe</h3>;
+      return <h3 className="text-center">Add Recipe</h3>;
     }
-}
+  }
 
-return (
-    <div className='container'>
-        <div className='row'>
-            <div className='card col-md-6 offset-md-3 offset-md-3 mt-4'>
-                {pageTitle()}
-                <div className='card-body'>
-                    <form>
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>Recipe name:</label>
-                            <input
-                                type='text'
-                                placeholder='Enter recipe name'
-                                name='recipeName'
-                                value={recipeName}
-                                className={`form-control ${errors.recipeName ? 'is-invalid' : ''}`}
-                                onChange={(e) => setRecipeName(e.target.value)}
-                            />
-                            {errors.recipeName && <div className='invalid-feedback'>{errors.recipeName}</div>}
-                        </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="card col-md-6 offset-md-3 offset-md-3 mt-4">
+          {pageTitle()}
+          <div className="card-body">
+            <form>
+              <div className="form-group mb-2">
+                <label className="form-label">Recipe name:</label>
+                <input
+                  type="text"
+                  placeholder="Enter recipe name"
+                  name="recipeName"
+                  value={recipeName}
+                  className={`form-control ${errors.recipeName ? "is-invalid" : ""}`}
+                  onChange={(e) => setRecipeName(e.target.value)}
+                />
+                {errors.recipeName && (
+                  <div className="invalid-feedback">{errors.recipeName}</div>
+                )}
+              </div>
 
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>Ingredients:</label>
-                            <input
-                                type='text'
-                                placeholder='Enter ingredients'
-                                name='ingredients'
-                                value={ingredients}
-                                className={`form-control ${errors.ingredients ? 'is-invalid' : ''}`}
-                                onChange={(e) => setIngredients(e.target.value)}
-                            />
-                            {errors.ingredients && <div className='invalid-feedback'>{errors.ingredients}</div>}
-                        </div>
-                       
-                        <div className='form-group mb-2'>
-                                <label className='form-label'>Steps:</label>
-                                <input
-                                    type='text'
-                                    placeholder='Enter steps'
-                                    name='steps'
-                                    value={steps}
-                                    className={`form-control ${errors.steps ? 'is-invalid' : ''}`}
-                                    onChange={(e) => setSteps(e.target.value)}
-                                />
-                                {errors.steps && <div className='invalid-feedback'>{errors.steps}</div>}
-                            </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Ingredients:</label>
+                <input
+                  type="text"
+                  placeholder="Enter ingredients"
+                  name="ingredients"
+                  value={ingredients}
+                  className={`form-control ${errors.ingredients ? "is-invalid" : ""}`}
+                  onChange={(e) => setIngredients(e.target.value)}
+                />
+                {errors.ingredients && (
+                  <div className="invalid-feedback">{errors.ingredients}</div>
+                )}
+              </div>
 
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>Add image:</label>
-                            <input
-                                type='text'
-                                placeholder='Add image'
-                                name='imagePath'
-                                value={image}
-                                className={`form-control ${errors.image ? 'is-invalid' : ''}`}
-                                onChange={(e) => setImage(e.target.value)}
-                            />
-                            {errors.image && <div className='invalid-feedback'>{errors.image}</div>}
-                        </div>                        
+              <div className="form-group mb-2">
+                <label className="form-label">Steps:</label>
+                <input
+                  type="text"
+                  placeholder="Enter steps"
+                  name="steps"
+                  value={steps}
+                  className={`form-control ${errors.steps ? "is-invalid" : ""}`}
+                  onChange={(e) => setSteps(e.target.value)}
+                />
+                {errors.steps && (
+                  <div className="invalid-feedback">{errors.steps}</div>
+                )}
+              </div>
 
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>Category:</label>
-                            <select
-                                className={`form-control ${errors.recipeCategory ? 'is-invalid' : ''}`}
-                                value={categoryId}
-                                onChange={(e) => setCategoryId(e.target.value)}
-                            >
-                                <option value="Select category">Select category</option>
-                                {
-                                    categories.map(category =>
-                                        <option key={category.id} value={category.id}>{category.recipeCategory}</option>
-                                    )
-                                }
+              <div className="form-group mb-2">
+                <label className="form-label">Add image:</label>
+                <input
+                  type="text"
+                  placeholder="Add image"
+                  name="imageUrl"
+                  value={imageUrl}
+                  className={`form-control ${errors.imageUrl ? "is-invalid" : ""}`}
+                  onChange={(e) => setImageUrl(e.target.value)}
+                />
+                {errors.imageUrl && (
+                  <div className="invalid-feedback">{errors.imageUrl}</div>
+                )}
+              </div>
 
-                            </select>
-                            {errors.recipeCategory && <div className='invalid-feedback'>{errors.recipeCategory}</div>}
-                        </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Category:</label>
+                <select
+                  className={`form-control ${errors.recipeCategory ? "is-invalid" : ""}`}
+                  value={categoryId}
+                  onChange={(e) => setCategoryId(e.target.value)}
+                >
+                  <option value="Select category">Select category</option>
+                  {categories.map((category) => (
+                    <option key={category.id} value={category.id}>
+                      {category.recipeCategory}
+                    </option>
+                  ))}
+                </select>
+                {errors.recipeCategory && (
+                  <div className="invalid-feedback">
+                    {errors.recipeCategory}
+                  </div>
+                )}
+              </div>
 
-                        <button className='btn btn-secondary' onClick={saveOrUpdateRecipe}>Submit</button>
-                        <button className='btn btn-secondary mx-3' onClick={handleCancel}>Cancel</button>
-                    </form>
-                </div>
-            </div>
+              <button
+                className="btn btn-secondary"
+                onClick={saveOrUpdateRecipe}
+              >
+                Submit
+              </button>
+              <button className="btn btn-secondary mx-3" onClick={handleCancel}>
+                Cancel
+              </button>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-);
+  );
 };
 
 export default RecipeComponent;
