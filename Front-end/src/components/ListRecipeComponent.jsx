34730c34151db6ff71bf46cd7a4bfba045d9aa8f
Nerimantas7 <nerimantas.tarabilda@stud.techin.lt>
2024-12-19 15:33:38 +0200

    Removed accordion feature

--- Front-end/src/components/ListRecipeComponent.jsx
+++ Front-end/src/components/ListRecipeComponent.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { listRecipes } from "../services/RecipeService";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const ListRecipeComponent = () => {
   const [recipes, setRecipes] = useState([]);
@@ -21,81 +21,40 @@ const ListRecipeComponent = () => {
     <div>
       <div className="row row-cols-1 row-cols-md-3 g-1 mx-4 my-3">
         {recipes.map((recipe) => (
-          <div
-            key={recipe.id}
-            className="card mb-3 mx-auto"
-            style={{ maxWidth: "450px" }}
-          >
+          <div key={recipe.id} className="card mb-3 mx-auto" style={{ maxWidth: "450px" }} >
             {/* <div className="row g-0">
               <div className="col-md-4"> */}
-            <img
-              src={recipe.recipeImageUrl}
-              className="img-fluid rounded-start"
-              alt={recipe.recipeName}
-            />
-            {/* </div> */}
-            <div className="col-md-12">
-              <div className="card-body">
-                <h5 className="card-title">{recipe.recipeName}</h5>
-                <div className="accordion-item">
-                  <h2 className="accordion-header">
-                    <button
-                      className="accordion-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#panelsStayOpen-collapseOne"
-                      aria-expanded="true"
-                      aria-controls="panelsStayOpen-collapseOne"
-                    >
-                      <b>Ingredients:</b>
-                    </button>
-                  </h2>
-                  <div
-                    id="panelsStayOpen-collapseOne"
-                    className="accordion-collapse collapse show"
-                  >
-                    <div className="accordion-body">
-                      <p className="card-text">{recipe.recipeIngredients}</p>
-                    </div>
-                  </div>
+                <img src={recipe.recipeImageUrl} className="img-fluid rounded-start" alt={recipe.recipeName} />
+              {/* </div> */}
+              <div className="col-md-12">
+                <div className="card-body">
+                  <h5 className="card-title">{recipe.recipeName}</h5>
+                  <p className="card-text">{recipe.recipeIngredients}</p>
+                  <p className="card-text">{recipe.recipeSteps}</p>
+                  <button className="btn btn-outline-secondary" onClick={() => handleShowModal(recipe)}>Write a comment</button>
+                  <button className="btn btn-outline-secondary mx-3" onClick={() => handleShowModal(recipe)}>Read comments</button>
                 </div>
 
-                
-                <p className="card-text">{recipe.recipeSteps}</p>
-                <button
-                  className="btn btn-outline-secondary"
-                  onClick={() => handleShowModal(recipe)}
-                >
-                  Write a comment
-                </button>
-                <button
-                  className="btn btn-outline-secondary mx-3"
-                  onClick={() => handleShowModal(recipe)}
-                >
-                  Read comments
-                </button>
+                {/* {isAdmin && ( */}
+                  <div className="card-footer">
+                    <button
+                      type="button"
+                      className="btn btn-outline-secondary"
+                      onClick={() => updateRecipe(recipe.id)}
+                    >
+                      Update
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-outline-secondary mx-3"
+                      onClick={() => removeRecipe(recipe.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                {/* )} */}
               </div>
-
-              {/* {isAdmin && ( */}
-              <div className="card-footer">
-                <button
-                  type="button"
-                  className="btn btn-outline-secondary"
-                  onClick={() => updateRecipe(recipe.id)}
-                >
-                  Update
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-outline-secondary mx-3"
-                  onClick={() => removeRecipe(recipe.id)}
-                >
-                  Delete
-                </button>
-              </div>
-              {/* )} */}
             </div>
-          </div>
           // </div>
         ))}
       </div>
